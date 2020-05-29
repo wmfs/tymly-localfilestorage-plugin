1e6102a23d391d34007ca23a90ae1ddad07074cd
Jez Higgins <jez@jezuk.co.uk>
2020-05-29 09:55:50 +0100

    test(service): Boot config tests

--- test/boot-service-test.js
+++ test/boot-service-test.js
@@ -8,38 +8,59 @@ const LocalStorageService = require('../lib/components/services/localfilestorage
 
 describe('Boot localstorage service', () => {
   let localstorage
-  let options =
+  let options
+  const onBoot = () => { localstorage.boot(options) }
 
   beforeEach (() => {
+    console.log('yes')
+
     localstorage = new LocalStorageService()
 
     options = {
       bootedServices: {
         cloudstorage: {
+          registerProvider: () => { }
         }
       }
     }
   })
 
-  it('localstorage registers with cloudstorage on boot', () => {
-    let registered = false
-    options.bootedServices.cloudstorage.registerProvider =
+  describe('good boots', () => {
+    it('localstorage registers with cloudstorage on boot', () => {
+      let registered = false
+      options.bootedServices.cloudstorage.registerProvider =
         provider => { registered = (provider === localstorage) }
 
-    localstorage.boot(options)
-    expect(registered).to.be.true()
+      localstorage.boot(options)
+      expect(registered).to.be.true()
+    })
+
+    it('picks up filesystem root directory from config', () => {
+      localstorage.boot(options)
+      expect(localstorage.rootPath).to.be.a('string')
+    })
+
+    it('picks up filesystem root directory from environment', () => {
+      localstorage.boot(options)
+      expect(localstorage.rootPath).to.be.a('string')
+    })
   })
 
-  it('loudly fail if cloudstorage is not available', () => {
-    delete options.bootedServices.cloudstorage
-    const onBoot = () => { localstorage.boot(options) }
+  describe('bad boots', () => {
+    it('loudly fail if cloudstorage is not available', () => {
+      delete options.bootedServices.cloudstorage
 
-    expect(onBoot).to.throw(/can not register/i)
-  })
+      expect(onBoot).to.throw(/can not register/i)
+    })
 
-  it ("loudly fail if cloudstorage isn't right", () => {
-    const onBoot = () => { localstorage.boot(options) }
+    it ("loudly fail if cloudstorage isn't right", () => {
+      delete options.bootedServices.cloudstorage.registerProvider
 
-    expect(onBoot).to.throw(/can not register/i)
+      expect(onBoot).to.throw(/can not register/i)
+    })
+
+    it ('loudly fail if path config is missing', () => {
+      expect(onBoot).to.throw(/missing config/i)
+    })
   })
 })
