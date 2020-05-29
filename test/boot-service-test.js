249053fbc9bc99fa0ac72ec00cc026de17773c67
Jez Higgins <jez@jezuk.co.uk>
2020-05-29 11:09:58 +0100

    fix(service): Pick up config, then register

--- test/boot-service-test.js
+++ test/boot-service-test.js
@@ -9,11 +9,11 @@ const LocalStorageService = require('../lib/components/services/localfilestorage
 describe('Boot localstorage service', () => {
   let localstorage
   let options
-  const onBoot = () => { localstorage.boot(options) }
+  const onBoot = () => {
+    localstorage.boot(options)
+  }
 
   beforeEach (() => {
-    console.log('yes')
-
     localstorage = new LocalStorageService()
 
     options = {
@@ -21,46 +21,79 @@ describe('Boot localstorage service', () => {
         cloudstorage: {
           registerProvider: () => { }
         }
+      },
+      config: {
+        localstorage: {
+          rootPath: '/from/config'
+        }
       }
     }
+  }) // beforeEach
+
+  afterEach (() => {
+    delete process.env.TYMLY_LOCALSTORAGE_ROOTPATH
   })
 
   describe('good boots', () => {
-    it('localstorage registers with cloudstorage on boot', () => {
-      let registered = false
-      options.bootedServices.cloudstorage.registerProvider =
-        provider => { registered = (provider === localstorage) }
-
-      localstorage.boot(options)
-      expect(registered).to.be.true()
-    })
-
     it('picks up filesystem root directory from config', () => {
       localstorage.boot(options)
       expect(localstorage.rootPath).to.be.a('string')
     })
 
     it('picks up filesystem root directory from environment', () => {
+      delete options.config.localstorage
+      process.env.TYMLY_LOCALSTORAGE_ROOTPATH='/from/env'
+
       localstorage.boot(options)
-      expect(localstorage.rootPath).to.be.a('string')
+      expect(localstorage.rootPath).to.eql('/from/env')
+    })
+
+    it('prefer config to environment variable', () => {
+      process.env.TYMLY_LOCALSTORAGE_ROOTPATH='/from/env'
+
+      localstorage.boot(options)
+      expect(localstorage.rootPath).to.eql('/from/config')
+    })
+
+    it('localstorage registers with cloudstorage on boot', () => {
+      let registered = false
+      options.bootedServices.cloudstorage.registerProvider =
+        provider => { registered = (provider === localstorage) }
+
+      localstorage.boot(options)
+
+      expect(registered).to.be.true()
     })
   })
 
   describe('bad boots', () => {
+    it ('loudly fail if path config is missing', () => {
+      delete options.config.localstorage
+
+      expect(onBoot).to.throw(/could not configure/i)
+    })
+
+    it ("don't register if config is missing", () => {
+      delete options.config.localstorage
+
+      let registered = false
+      options.bootedServices.cloudstorage.registerProvider =
+        provider => { registered = (provider === localstorage) }
+
+      expect(onBoot).to.throw(/could not configure/i)
+      expect(registered).to.be.false()
+    })
+
     it('loudly fail if cloudstorage is not available', () => {
       delete options.bootedServices.cloudstorage
 
-      expect(onBoot).to.throw(/can not register/i)
+      expect(onBoot).to.throw(/can't find cloudstorage/i)
     })
 
     it ("loudly fail if cloudstorage isn't right", () => {
       delete options.bootedServices.cloudstorage.registerProvider
 
-      expect(onBoot).to.throw(/can not register/i)
-    })
-
-    it ('loudly fail if path config is missing', () => {
-      expect(onBoot).to.throw(/missing config/i)
+      expect(onBoot).to.throw(/doesn't have registerProvider/i)
     })
   })
 })
